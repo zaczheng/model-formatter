83019af1f2257235506911cac1934b68a3b40136
Zac Zheng <zac@zenpow.com>
2011-09-25 12:35:07 +0100

    added :unit option for currenty formatter which defaults to '&pound'

--- lib/formatters/format_currency.rb
+++ lib/formatters/format_currency.rb
@@ -2,22 +2,22 @@ require 'rubygems'
 require 'action_view'
 
 module Formatters
-	class FormatCurrency < Format
-		include ActionView::Helpers::NumberHelper
+  class FormatCurrency < Format
+    include ActionView::Helpers::NumberHelper
+      def initialize(options = {})
+        @precision = options[:precision] || 2
+        @unit = options[:unit] || "&pound;"
+      end
 
-		def initialize(options = {})
-			@precision = options[:precision] || 2
-		end
+      def from(value, options = {})
+        options = {:precision => @precision, :unit => @unit}.merge(options)
+  	number_to_currency(value.to_f / (10 ** @precision), options)
+      end
 
-		def from(value, options = {})
-		  options = {:precision => @precision}.merge(options)
-			number_to_currency(value.to_f / (10 ** @precision), options)
-		end
-
-		def to(str, options = {})
-			return nil if str.nil? or str.empty?
-			val = str.gsub(/[^0-9.\-]/, '').to_f
-			(val * (10 ** @precision)) unless val.nil?
-		end
-	end
+      def to(str, options = {})
+        return nil if str.nil? or str.empty?
+	val = str.gsub(/[^0-9.\-]/, '').to_f
+	(val * (10 ** @precision)) unless val.nil?
+      end
+    end
 end
