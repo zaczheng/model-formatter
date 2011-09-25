716fb1e1a8a29cfdc46aa4ab81073b55f5bb2836
Zac Zheng <zac@zenpow.com>
2011-09-25 12:30:48 +0100

    added flexible percision option for percent formatter
    100 => %100
    99.99 => %99.0 by default, precision is set to 1 by default
    
    This solves the issue of percentages like: %100.000 etc. which look kind
    of dumb

--- lib/formatters/format_percent.rb
+++ lib/formatters/format_percent.rb
@@ -1,15 +1,22 @@
 require 'rubygems'
 require 'action_view'
 
+# Customised: added :flexible_precision i.e. 100.0 will be 100% not 100.0%. 1.5 will be 1.5%
 module Formatters
 	class FormatPercent < Format
 		include ActionView::Helpers::NumberHelper
 
 		def initialize(options = {})
+      @precision = options[:precision] || 1
 		end
 
 		def from(value, options = {})
-			number_to_percentage value, options
+		  if options[:flexible_precision] && value.to_i == value
+		    @precision = 0
+		  end
+		  
+      options = {:precision => @precision}.merge(options)
+		  number_to_percentage value, options
 		end
 
 		def to(str, options = {})
